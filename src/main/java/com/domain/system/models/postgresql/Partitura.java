a8f02bc183d343e7172e4f082b0ffb2877cd0018
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2023-11-14 18:23:14 -0600

    Commit Reformat Partitura

--- src/main/java/com/domain/system/models/postgresql/Partitura.java
+++ src/main/java/com/domain/system/models/postgresql/Partitura.java
@@ -8,10 +8,8 @@ import com.domain.system.models.Auditable;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.google.common.io.ByteStreams;
 
-import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,46 +32,35 @@ import lombok.NoArgsConstructor;
 //@RequiredArgsConstructor //Genera un constructor por cada parámetro de uso especial final o no nulo
 @AllArgsConstructor // Genera un cosntructor para cada parámetro finales o no nulos
 public class Partitura extends Auditable implements Serializable {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // GenerationType.IDENTITY AutoIncrement MYSQL MariaDB
-	@Column(name = "idPartitura",updatable = false, nullable = false)
+	@Column(name = "idPartitura", updatable = false, nullable = false)
 	private Long id;
-	
+
 	private String instrumento;
-	
+
 	@Lob
-	//@Basic(fetch = FetchType.EAGER)
-    private byte[] partituraPDF;
-	
+	// @Basic(fetch = FetchType.EAGER)
+	private byte[] partituraPDF;
+
 	@Lob
-	//@Basic(fetch = FetchType.EAGER)
-    private byte[] vistaPreviaPDF;
-	
+	// @Basic(fetch = FetchType.EAGER)
+	private byte[] vistaPreviaPDF;
+
 	@Lob
-	//@Basic(fetch = FetchType.EAGER)
-    private byte[] xml;
-	
+	// @Basic(fetch = FetchType.EAGER)
+	private byte[] xml;
+
 	@ManyToOne
-    @JoinColumn(name = "idObra")
+	@JoinColumn(name = "idObra")
 	@JsonBackReference
-    private Obra obra;
-	
-	@Override
-	public int hashCode() {
-		int result = super.hashCode(); // Llamar al hashCode de la superclase si es necesario
+	private Obra obra;
 
-		// Lógica específica para calcular hashCode de campos de MiClase
-		result = 31 * result + (id != null ? id.hashCode() : 0);
-		result = 31 * result + (instrumento != null ? instrumento.hashCode() : 0);
-
-		return result;
-	}
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	public void setPartituraPDFFromInputStream(InputStream inputStream) throws IOException {
-        this.partituraPDF = ByteStreams.toByteArray(inputStream);
-    }
+		this.partituraPDF = ByteStreams.toByteArray(inputStream);
+	}
 
 }
