6c1938f841815f314055fe40d0a15e1575b10039
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-12-13 18:30:11 -0600

    Commit Docker

--- src/test/java/com/domain/system/SystemApplicationTests.java
+++ src/test/java/com/domain/system/SystemApplicationTests.java
@@ -3,7 +3,9 @@ package com.domain.system;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.system.domain.SystemApplication;
+
+@SpringBootTest(classes = SystemApplication.class)
 class SystemApplicationTests {
 
 	@Test
