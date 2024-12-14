2ea473d0452ea28b22bb89515bceda0fdfafd8e5
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-12-13 18:40:53 -0600

    Docker jar

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO roles (rol,createdat,modifiedat) VALUES ('Administrador','1900-01-01 00:00:00.123','1900-01-01 00:00:00.123'),('Editor','1900-01-01 00:00:00.123','1900-01-01 00:00:00.123'),('Lector','1900-01-01 00:00:00.123','1900-01-01 00:00:00.123')
+ON CONFLICT (rol) DO NOTHING;
+INSERT INTO usuarios (nombres, primerapellido, segundoapellido, username, email, password, estatus, estatusbloqueo, createdat, modifiedat) VALUES ('ADMINISTRADOR', 'ADMINISTRADOR', 'ADMINISTRADOR', 'ADMINISTRADOR', 'administrador@domain.com', '$2a$10$2cuCFicESUZLGlyh/35oxuKz7rt98VAswbQGpqZlRxHnc8BIplS8O' , true, true, '1900-01-01 00:00:00.123','1900-01-01 00:00:00.123'),('SUPERVISOR', 'SUPERVISOR', 'SUPERVISOR', 'SUPERVISOR', 'supervisor@domain.com', '$2a$10$2cuCFicESUZLGlyh/35oxuKz7rt98VAswbQGpqZlRxHnc8BIplS8O' , true, true, '1900-01-01 00:00:00.123','1900-01-01 00:00:00.123'),('USUARIO', 'USUARIO', 'USUARIO', 'USUARIO', 'usuario@domain.com', '$2a$10$2cuCFicESUZLGlyh/35oxuKz7rt98VAswbQGpqZlRxHnc8BIplS8O' , true, true, '1900-01-01 00:00:00.123','1900-01-01 00:00:00.123')
+ON CONFLICT (email) DO NOTHING;
+INSERT INTO usuariosroles (idusuario, idrol) VALUES (1,1),(1,2),(1,3),(2,1),(2,2),(2,3),(3,1),(3,2),(3,3)
+ON CONFLICT (idusuario, idrol) DO NOTHING;
