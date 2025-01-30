9bfabb53b81e442f4a2c4238f7399cb9594cf3aa
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2025-01-29 23:25:34 -0600

    Copy MarcadeAgua.png

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM amazoncorretto:21-alpine-jdk
 
-COPY target/kamabye-0.0.1-SNAPSHOT.jar app.jar
+COPY target/kamabye-0.0.1-SNAPSHOT.jar /app.jar
+COPY src/main/resources /app/BOOT-INF/classes/
 
 ENTRYPOINT [ "java", "-jar", "/app.jar" ]
--- src/main/java/com/system/domain/services/ResourcesService.java
+++ src/main/java/com/system/domain/services/ResourcesService.java
@@ -2,45 +2,46 @@ package com.system.domain.services;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class ResourcesService {
-	
 	
 	public BufferedImage getMarcaDeAguaBufferedImage() {
 		
 		try {
 			Resource resource = new ClassPathResource("files/marcaDeAgua.png");
-
-            InputStream inputStream = resource.getInputStream();
-            
-            BufferedImage marcadeagua = javax.imageio.ImageIO.read(inputStream);
-
-            return marcadeagua;
-		}
-		catch(Exception e) {
+			
+			InputStream inputStream = resource.getInputStream();
+			
+			BufferedImage marcadeagua = javax.imageio.ImageIO.read(inputStream);
+			
+			return marcadeagua;
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
-public File getMarcaDeAguaFile() {
+	public File getMarcaDeAguaFile() {
 		
 		try {
-			Resource resource = new ClassPathResource("files/marcaDeAgua.png");
-
-            return resource.getFile();
+			ClassPathResource resource = new ClassPathResource("files/marcaDeAgua.png");
+			System.out.println(resource.getPath());
+			
+			return resource.getFile();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-		catch(Exception e) {
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-
+	
 }
